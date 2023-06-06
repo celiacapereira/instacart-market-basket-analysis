5196c5961e05b3d63bf7827ab467c07abd732ef2
celiacapereira <ccap98@gmail.com>
2023-06-06 15:11:04 +0100

    no commentns

--- models/staging/stg_aisle.sql
+++ models/staging/stg_aisle.sql
@@ -1,18 +1,4 @@
--- with source as (
 
---     select * from {{ source('market_basket', 'AISLE') }}
-
--- ),
--- renamed as (
-
---     select
---     AISLE_ID,
---     AISLE
---     from source
-
--- )
-
--- select * from renamed
 
 with source as (
 
