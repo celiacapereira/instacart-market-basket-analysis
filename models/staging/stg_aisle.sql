8972412d3d29d566cebf0e286d670c1c60e7efc2
celiacapereira <ccap98@gmail.com>
2023-06-06 15:05:04 +0100

    testing select * stg_aisle.sqç

--- models/staging/stg_aisle.sql
+++ models/staging/stg_aisle.sql
@@ -1,13 +1,29 @@
+-- with source as (
+
+--     select * from {{ source('market_basket', 'AISLE') }}
+
+-- ),
+-- renamed as (
+
+--     select
+--     AISLE_ID,
+--     AISLE
+--     from source
+
+-- )
+
+-- select * from renamed
+
 with source as (
 
-    select * from {{ source('market_basket', 'AISLE') }}
+    select AISLE_ID,
+    AISLE from {{ source('market_basket', 'AISLE') }}
 
 ),
 renamed as (
 
     select
-    AISLE_ID,
-    AISLE
+    *
     from source
 
 )
